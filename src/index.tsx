10d7c35f773fc6935d5baaae44f30bf3b3110b2a
resforx <139689090+resforx@users.noreply.github.com>
2024-05-10 11:52:48 +0900

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,22 @@
 /* testing */
 import react from 'react';
 
-export function Button({ text, onclick, style, className }:
-    { text: string, onclick?: react.MouseEventHandler<HTMLButtonElement> | undefined, style?: react.CSSProperties | undefined, className?: string | undefined }) {
+class Button {
+    static default({ text, onclick, style, className }: 
+        { text?: string, onclick?: react.MouseEventHandler<HTMLButtonElement> | undefined, style?: react.CSSProperties | undefined, className?: string | undefined }
+    ) {
         return (
             <button onClick={onclick} style={style} className={className}>
                 {text}
             </button>
         );
-}
+    }
 
-/* test finished */
+    static onElement({ element, onclick, style, className }: {
+        element: JSX.Element | string, onclick?: react.MouseEventHandler<HTMLButtonElement> | undefined, style?: react.CSSProperties | undefined, className?: string | undefined
+    }) {
+        <button onClick={onclick} style={style} className={className}>
+            {element}
+        </button>
+    }
+}
